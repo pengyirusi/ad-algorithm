9b355ecf58b1ffddeefc149c5683b96e1ac17d86
weiyupeng1 <weiyupeng1@xiaomi.com>
2021-09-25 20:21:39 +0800

    0.0

---
+++ tensorflow/primary/keras/image_classification.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pylab as plt
+
+# 导入数据集 Fashion MNIST
+# 10 个类 70000 个灰度图 60000 + 10000
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# # 查看数据格式
+# print(train_images.shape)
+# print(test_images.shape)
+# print(len(train_labels))
+# print(len(test_labels))
+# print(train_labels)
+
+# # 查看第一个图片
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+# 归一化处理
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# # 查看前 25 个图像，查看是否正确
+# plt.figure(figsize=(10, 10))
+# for i in range(25):
+#     plt.subplot(5, 5, i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+# 构建模型
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])
+
+# 编译模型
+model.compile(optimizer='adam',
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# 训练模型
+model.fit(train_images, train_labels, epochs=1)
+
+# 评估模型
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy: ', test_acc*100, '%')
+'''
+    过拟合：在评估模型时，训练准确率和测试准确率之间的差距
+'''
+
+# 看一眼预测结果的输出
+# predictions = model.predict(test_images)
+# print('logits: ', predictions[0])
+'''
+    logits：softmax 前面的那层，概率相加不等于 1
+    softmax：都在 0-1 之间，且相加等于 1
+'''
+probability_model = keras.Sequential([model, keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
+# print('softmax: ', predictions[0])
+
+# # np.argmax 返回数组中最大数的坐标
+# print(class_names[np.argmax(predictions[0])])
+
+# image with right or not by color
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         np.max(predictions_array) * 100,
+                                         class_names[true_label],
+                                         color=color))
+
+# softmax 每类的概率柱状图
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot=plt.bar(range(10), predictions_array, color='#666666')
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+# 以
